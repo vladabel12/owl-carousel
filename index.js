44096d2eaa39d356a1e6c9370b1c85bb8251f496
vladabel12 <vladabel12@gmail.com>
2024-03-04 14:21:01 +0200

    owl-carousel

---
+++ index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:1500,
+        autoplayHoverPause:false,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+            }
+        }
+    });
+  });
